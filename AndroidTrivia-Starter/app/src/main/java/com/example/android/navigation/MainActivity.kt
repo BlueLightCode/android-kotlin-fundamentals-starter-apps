1c4ac6109b4599349fc005a4c7efb4db81771e12
Jarvis <jarvistbonnerac@gmail.com>
2020-01-21 13:24:28 -0600

    Completed 03.2 Task 9

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,6 +19,8 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,8 +28,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // Grabs the Navigation Controller
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        // sets up the Acion Bar for the navController in this activity
+        NavigationUI.setupActionBarWithNavController(this,navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {  // enables the Arrow near the top
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
     // TODO (01) Create the new TitleFragment
     // Select File->New->Fragment->Fragment (Blank)
 
