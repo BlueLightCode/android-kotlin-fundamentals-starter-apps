c0e6cb839a4694f2d605ec493baff892fbf5c959
Jarvis <jarvistbonnerac@gmail.com>
2020-02-03 11:14:55 -0600

    Completed Lesson 8.2 2/3

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -39,10 +39,10 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
-    private val _property = MutableLiveData<MarsProperty>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    val property: LiveData<MarsProperty>
-        get() = _property
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
 
     private var viewModelJob = Job()
 
@@ -70,9 +70,8 @@ class OverviewViewModel : ViewModel() {
                 _response.value =
                     "Success: ${listResult.size} Mars properties retrieved"
 
-                if (listResult.size > 0) {
-                    _property.value = listResult[0]
-                }
+                _properties.value = listResult
+                
             } catch (e: Exception) {
                 _response.value = "Failure: ${e.message}"
             }
