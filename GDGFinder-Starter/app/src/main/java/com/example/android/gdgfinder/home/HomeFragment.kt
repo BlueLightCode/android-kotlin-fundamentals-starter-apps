142516e379fbd1d509cf80dcd8985402b3a9ce25
Jarvis <jarvistbonnerac@gmail.com>
2020-02-05 10:42:11 -0600

    Completed 10.2 1/5

--- GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -22,7 +22,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -33,12 +39,35 @@ class HomeFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
 
+    private val _navigateToSearch = MutableLiveData<Boolean>()
+    val navigateToSearch: LiveData<Boolean>
+        get() = _navigateToSearch
+
+    fun onFabClicked() {
+        _navigateToSearch.value = true
+    }
+
+    fun onNavigatedToSearch() {
+        _navigateToSearch.value = false
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+            Observer<Boolean> { navigate ->
+                if(navigate) {
+                    val navController = findNavController()
+                    navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                    viewModel.onNavigatedToSearch()
+                }
+            })
 
         return binding.root
     }
